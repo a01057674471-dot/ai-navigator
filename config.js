92e824b07b1b849e42eb4fd5bd6e876af559a562
a01057674471-dot <a01057674471@gmail.com>
2026-07-20 17:42:33 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.AI_NAVIGATOR_CONFIG = {
+  supabaseUrl: 'https://bbsbhdsevvfjyhbwwacc.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Fp2Bt6x4oaQeWfbtc5bFxQ_eAdNHefc',
+  adminEmails: []
+};
